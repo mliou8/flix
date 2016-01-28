9d84eb01479e75d9a569151fc8ecde4715a153be
mliou8 <chunghanliou@gmail.com>
2016-01-28 16:47:42 -0500

    switching over to master

--- server/search.js
+++ server/search.js
@@ -1,50 +1,51 @@
 var fs = require('fs');
 var path = require('path');
 var dir = require('node-dir');
-var glob = require("glob")
-
-// var walk    = require('walk')
-// var walker  = walk.walk("/tmp", { followLinks: false })
-console.log("SUP")
-
-function parse (fileName){
-    var quality = /\d{3,4}p/;
-    var season = /[Ss](\d{1,2})/;
-    var episode = /[Ee](\d{1,2})/;
-    var title = /[^SE(\d{1,2})]*/
-
-    var obj = {};
-
-    obj.quality = fileName.match(quality).shift();
-    obj.title = fileName.match(title).shift().split('.');
-    obj.title = obj.title.splice(0, obj.title.length-1).join(" ")
-    obj.season = fileName.match(season).shift() || 'no season';
-    console.log(obj.title)
-    obj.episode = fileName.match(episode).shift() || 'no episode';
-    return obj;
-}
-// options is optional
-glob("**/*.mkv", {cwd: '/Users/mliou' }, function (er, files) {
-  // files is an array of filenames.
-  // If the `nonull` option is set, and nothing
-  // was found, then files is ["**/*.js"]
-  // er is an error object or null.
-  var shows = []
-  files.forEach(function (data) {
-  	var string = data.split('/')[1];
-  	var show = parse(string);
-  	show.filePath = data;
-  	shows.push(show);
-  })
-  console.log(shows);
-  return shows;
-})
+var glob = require("multi-glob").glob;
+var find = require('findit');
 
 
+//Set up variables to find user's homedir
+
+var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
+var finder = find();
+
+finder.on('file', function (file, stat) {
+    console.log(file);
+});
+
+
+//RIP Glob
+// var moviePaths = ["**/*.mkv", "**/*.avi", "**/*.mp4",
+// "**/*.264", "**/*.mov", "**/*.dv4", "**/*.dvr", "**/*.flv", "**/*.gifv"];
+
+// function filePathJoin (fileName) {
+// 	var fileName = fileName.split('/').slice(0, -1).join('/');
+// 	var finalFile = path.join(fileName);
+// 	return finalFile;
+// }
+// // var str = 'TV Shows/app/home/the.great.british.bake.off.s06e01.cake.720p.hdtv.x264-c4tv.mkv';
+// // filePathJoin(str);
+
+// glob(moviePaths, {cwd: homedir }, function (er, files) {
+//   // files is an array of filenames.
+//   // If the `nonull` option is set, and nothing
+//   // was found, then files is ["**/*.js"]
+//   // er is an error object or null.
+//   var shows = []
+//   files.forEach(function (data) {
+//   	var showString = data.split('/')[1];
+//   	// var showString = parseTV(showString);
+//   	//Saves standardized filepath
+//   	showString.filePath = filePathJoin(data);
+//   	shows.push(data);
+//   })
+//   console.log("shows ", shows)
+//   return shows;
+// })
 // Rick.and.Morty.S02E01.720p.WEB-DL.x264.AC3-nopan.mkv
 
-
-
+//RIP Native implementation
 // //Set up a walk through function
 // var walk = function(dir, done) {
 //   var results = [];
@@ -74,3 +75,6 @@ glob("**/*.mkv", {cwd: '/Users/mliou' }, function (er, files) {
 //   if (err) throw err;
 //   console.log(results);
 // });
+
+
+module.exports('search');
