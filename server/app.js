000ee23b2901926b5c45fb866888d2764dc0f86b
mliou8 <chunghanliou@gmail.com>
2016-02-01 17:52:37 -0500

    cleaned up redundant code in app.js using multer middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var expApp = express();
 var path = require('path');
-var multer  =   require('multer');
 
 module.exports = expApp
 
@@ -11,27 +10,7 @@ expApp.use('/recently_added', require('./routes/recently_added'))
 expApp.use('/movies', require('./routes/movies'))
 expApp.use('/shows', require('./routes/shows'))
 
-expApp.use(function(err, req, res, next){
+expApp.use(function(err, req, res, next) {
   console.log(err);
   res.status(500).send(err.message);
 })
-
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
-  }
-});
-
-var upload = multer({ storage : storage}).single('userPhoto');
-
-expApp.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
