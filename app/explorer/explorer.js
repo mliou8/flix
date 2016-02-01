476fa25443e0df5a6d2ce261727d5532771ae1a8
mliou8 <chunghanliou@gmail.com>
2016-02-01 17:47:27 -0500

    updated parsing through

--- app/explorer/explorer.js
+++ app/explorer/explorer.js
@@ -1,53 +1,62 @@
+app.controller('Explorer', function($scope, $element, vidConstants) {
+  // Code to make the 'file holder div' take in
+  // file path names
+  var homedir = (process.platform === 'win32') ? process.env.HOMEPATH :
+    process.env.HOME;
+  const shell = require('electron').shell;
+  // var finder = require('./server/algorithms/search.js')
+  var fs = require('fs');
+  var path = require('path');
+  var find = require('findit');
+  var parseVideo = require('video-name-parser');
 
+  //Button called "browse", opens homedirectory of user
+  $scope.openHome = function() {
+    return shell.showItemInFolder(homedir);
+  }
 
-app.controller('Explorer', function ($scope) {
-      // Code to make the 'file holder div' take in
-      // file path names
-      var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
-      const shell = require('electron').shell;
-      // var finder = require('./server/algorithms/search.js')
-      var fs = require('fs');
-      var path = require('path');
-      var find = require('findit');
-      var parseVideo = require('video-name-parser');
+  // --------------------------
+  // Code that allows you to recurse through the directories
+  //provided and return file names that end with certain exts
 
-
-      //Button called "browse", opens homedirectory of user
-      $scope.openHome = function () {
-        return shell.showItemInFolder(homedir);
-      }
-
-       var holder = document.getElementById('fileholder');
-       holder.ondragover = function () { this.className = 'hover'; return false; };
-       holder.ondragleave = function () { this.className = ''; return false; };
-       holder.ondrop = function (e) {
-         e.preventDefault();
-         for (var i = 0; i < e.dataTransfer.files.length; ++i) {
-           console.log(e.dataTransfer.files[i].path);
+  var holder = document.getElementById('fileholder');
+  holder.ondragover = function() {
+    this.className = 'hover';
+    return false;
+  };
+  holder.ondragleave = function() {
+    this.className = '';
+    return false;
+  };
+  holder.ondrop = function(e) {
+    var dataPathArray = [];
+    var soln = [];
+    e.preventDefault();
+    for (var i = 0; i < e.dataTransfer.files.length; ++i) {
+      dataPathArray.push(e.dataTransfer.files[i].path);
+    }
+    dataPathArray.forEach(function(data) {
+      var finder = find(data);
+      finder.on('file', function(file, stat) {
+        if (vidConstants.vidExtensions.indexOf(path.extname(file)) !==
+          -1) {
+          var parsed = parseVideo(file);
+          soln.push(parsed);
         }
-        var dataPath = e.dataTransfer.files[0].path;
-        var finder = find(dataPath);
-
-      finder.on('file', function (file, stat) {
-           var showString = file.split('/');
-           var temp = showString[showString.length - 1].split(".");
-            if (vidExtensions.indexOf(temp[temp.length - 1]) !== -1) {
-              soln.push(parseVideo(file));
-            }
-            console.log("Thank you for your patience!");
-         })
-      finder.on('end', function (file, stat) {
-           console.log('soln', soln)
-         })
-         return false;
-       };
-
-
-
-       var vidExtensions = ['mkv', 'avi', 'mov', 'gifv', 'flv'];
-       var soln = [];
-
-      //  finder.setFinder(e.dataTransfer.files[i].path);
-
-
+        console.log("Thank you for your patience!");
+      })
+      finder.on('end', function(file, stat) {
+        // soln.forEach()
+        console.log('soln', soln)
+      })
+    })
+    return false;
+  };
 });
+
+
+app.factory('vidConstants', function() {
+  return {
+    vidExtensions: ['.mkv', '.avi', '.mov', '.gifv', '.flv']
+  }
+})
