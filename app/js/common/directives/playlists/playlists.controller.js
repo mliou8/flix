e9499f9ca7bd8ee80d99ee86b80fa62b42b2bde4
mliou8 <chunghanliou@gmail.com>
2016-02-04 17:43:41 -0500

    Basic playlist adding functionality

--- app/js/common/directives/playlists/playlists.controller.js
+++ app/js/common/directives/playlists/playlists.controller.js
@@ -1,10 +1,15 @@
-angApp.controller('PlaylistsCtrl', function($scope) {
+angApp.controller('PlaylistsCtrl', function($scope, Storage) {
 
   $scope.playlists = ['playlist1', "playlist2", "playlist3"];
-  $scope.newInputs = [];
-  $scope.addNewPlaylist = function() {
-    $scope.newInputs.push("newitem");
+  $scope.showForm = false;
+  $scope.addNewForm = function() {
+    $scope.showForm = true;
   }
-
+  $scope.createPlaylist = function() {
+    console.log($scope.name);
+    $scope.showForm = false;
+    Storage.findOrCreate($scope.name)
+      .then(result => console.log(result));
+  }
 
 })
