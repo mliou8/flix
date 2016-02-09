858a602b5b04a1a7727aa8d835f1734bac9f6f11
Matthew Kim <matthewkim93@gmail.com>
2016-02-09 16:03:55 -0500

    styling man

--- app/js/common/addRemote/addremote.controller.js
+++ app/js/common/addRemote/addremote.controller.js
@@ -1,5 +1,5 @@
 angApp.controller('AddRemoteCtrl', function($rootScope, $state, $scope, Storage) {
-  $scope.ipAddress;
+  $scope.ipAddress = 'http://192.168.2.89:3000'
 
   $scope.getRemote = function() {
     console.log($scope.ipAddress);
