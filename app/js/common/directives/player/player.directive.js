d1e4825e526f2d38ee007f8b59640d0ae878f758
MarkWaldron <markewaldron@gmail.com>
2016-03-08 13:23:02 -0500

    Staged cast function with basic logic

--- app/js/common/directives/player/player.directive.js
+++ app/js/common/directives/player/player.directive.js
@@ -1,30 +1,51 @@
 angApp.directive('player', function($rootScope) {
-	return {
-		restrict: 'E',
-		templateUrl: './app/js/common/directives/player/player.html',
-		link: function(scope, element, attribute) {
-			var $ = require('jquery');
-			var wjs = require('wcjs-player');
-			var chromecastjs = require('chromecast-js');
-			window.devices = [];
-			var browser = new chromecastjs.Browser();
+  return {
+    restrict: 'E',
+    templateUrl: './app/js/common/directives/player/player.html',
+    link: function(scope, element, attribute) {
+      var $ = require('jquery');
+      var wjs = require('wcjs-player');
+      var chromecastjs = require('chromecast-js');
+      window.devices = [];
+      var splitFilePath = scope.filePath.split('.');
+      var extension = splitFilePath[splitFilePath.length-1];
+      var browser = new chromecastjs.Browser();
+      window.casting = false;
 
-
-      window.player = new wjs("#player").addPlayer({ autoplay: true });
+      window.player = new wjs('#player').addPlayer({ autoplay: true });
       $rootScope.hideSidebar = true;
       player.addPlaylist(scope.filePath);
-      $(".wcp-surface").append( '<div class="wcp-titlebar"><button class="backbutton" onclick="goBack(window.player)"><i class="fa fa-arrow-left fa-2x"></i></button></div>');
-      window.goBack = function(player){
+      console.log(scope.filePath);
+      $('.wcp-surface').append('<div class="wcp-titlebar"><button class="backbutton" onclick="goBack(window.player)"><i class="fa fa-arrow-left fa-2x"></i></button></div>');
+      window.goBack = function(player) {
         $rootScope.hideSidebar = false;
         player.clearPlaylist();
         scope.goHome();
       };
 
-			browser.on('deviceOn', function(device){
-				$(".wcp-toolbar").append('<div class="wcp-button wcp-right"><button><img src="./app/assets/icons/ic_cast_grey.png"></button></div>');
-			});
+      browser.on('deviceOn', function(device) {
+        $('.wcp-toolbar').append('<div class="wcp-button wcp-right"><button onclick="cast()"><img src="./app/assets/icons/ic_cast_grey.png"></button></div>');
+        devices.push(device);
+        console.log(extension);
+      });
 
-			// browser.on('deviceOn', function(device){
+      window.cast = function() {
+        var device = devices[0];
+        if (casting == false) {
+          device.connect();
+          device.on('connected', function() {
+            if(extension.toLowerCase() == 'mp4') {
+              
+            } else {
+
+            }
+          });
+        } else {
+          device.stop();
+          casting = false;
+        }
+      };
+      // browser.on('deviceOn', function(device){
       //   device.connect()
       //   device.on('connected', function(){
       //
@@ -36,32 +57,32 @@ angApp.directive('player', function($rootScope) {
 
 
 
-			var Annyang = require('annyang')
-			// Let's define our first command. First the text we expect, and then the function it should call
-		  var commands = {
-		    'video pause': function() {
-		      player.togglePause();
-		    },
-				'video paws': function(){
-					player.togglePause();
-				},
-				'video play': function() {
-		      player.togglePause();
-		    },
-				'mute': function(){
-					player.toggleMute();
-				},
-				'*allSpeech': function(allSpeech){
-					console.log(allSpeech)
-				}
-		  };
+      var Annyang = require('annyang');
+      // Let's define our first command. First the text we expect, and then the function it should call
+      var commands = {
+        'video pause': function() {
+          player.togglePause();
+        },
+        'video paws': function() {
+          player.togglePause();
+        },
+        'video play': function() {
+          player.togglePause();
+        },
+        'mute': function() {
+          player.toggleMute();
+        },
+        '*allSpeech': function(allSpeech) {
+          console.log(allSpeech);
+        }
+      };
 
-		  // Add our commands to annyang
-		  annyang.addCommands(commands);
+      // Add our commands to annyang
+      annyang.addCommands(commands);
 
-		  // Start listening. You can call this here, or attach this call to an event, button, etc.
-		  annyang.start();
+      // Start listening. You can call this here, or attach this call to an event, button, etc.
+      annyang.start();
 
-		}
-	};
+    }
+  };
 });
